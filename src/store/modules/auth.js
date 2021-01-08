c010ea5d78f8c7f6d70753ff869d5663ca6cd6cc
Patrick Kluwig <kluwigpa@netspoc-entw.test-labor>
2021-01-08 09:27:00 +0100

    bugs

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -9,7 +9,9 @@ export default {
     },
     getters: {
         getLoggedIn: state => {
-            return state.loggedIn;
+            console.log('MY STATE');
+            console.log(state.loggedIn);
+            return state.loggedIn || localStorage.getItem('loggedIn');
         },
         getLoginError: state => {
             return state.loginError;
@@ -18,6 +20,7 @@ export default {
     mutations: {
         SET_LOGGED_IN: (state, loggedIn) => {
             state.loggedIn = loggedIn;
+            localStorage.setItem('loggedIn',loggedIn);
         },
         SET_LOGIN_ERROR: (state, loginError) => {
             state.loginError = loginError;
@@ -27,8 +30,10 @@ export default {
         setLoggedIn({ commit }, bool) {
             commit('SET_LOGGED_IN', bool);
         },
-        requestLoggedIn({ commit }) {
-            return Vue.axios.get('/set')
+        requestLoggedIn({getters}) {
+           
+            return getters.getLoggedIn;
+            /* return Vue.axios.get('/set')
                 .then(function (response) {
                     let recurare = response.data.success;
                     commit('SET_LOGGED_IN', recurare);
@@ -36,12 +41,12 @@ export default {
                 })
                 .catch(function () {
                     commit('SET_LOGGED_IN', false);
-                });
+                }); */
         },
-        loginUser({ dispatch, commit }, creds) {
+        loginUser({ commit }, creds) {
             return Vue.axios.post('/login_vue', creds.data).then(res => {  
                 if(res.data.success) {
-                    dispatch('setLoggedIn', true);
+                    commit('SET_LOGGED_IN', true);
                     commit('SET_LOGIN_ERROR',false);
                     router.push('/services');
                 } else {
