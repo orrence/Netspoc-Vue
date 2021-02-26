e0889cb48f3a04445a23920255fd1568b7b6e699
Patrick Kluwig <kluwigpa@netspoc-entw.test-labor>
2021-02-26 13:42:41 +0100

    dff bug

--- src/views/Diff.vue
+++ src/views/Diff.vue
@@ -73,7 +73,7 @@ export default {
   watch: {
     active: {
       deep: true,
-      handler(val, oldVal) {
+      handler() {
   
         this.getOlderPolicies();
         this.oldPolicy = null;
