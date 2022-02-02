102badeebd062a95114b4d632fa945f5dae8c60a
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2022-02-02 11:34:08 +0100

    Fix broken Netze-->Ressourcen
    When mergin branch "berechtigungen" somehow accidentally
    "passOnSelectionUpdate" was overwritten, changing from
    
        passOnSelectionUpdate(data) {
          this.$emit(
            "selectionUpdate",
            data.map((row) => row.name)
          );
        },
    
    to
    
        passOnSelectionUpdate(data) {
          this.$emit(
            "selectionUpdate",
            data.map((row) => row.email)
          );
        },
    
    As a consequence, the data passed on was always empty, leading
    to empty list of "selected_networks" to be sent to backend
    leading to empty list as a return value.
    
    This fixes issue #39.

--- src/components/tables/Network/NetworkTable.vue
+++ src/components/tables/Network/NetworkTable.vue
@@ -105,7 +105,7 @@ export default {
     passOnSelectionUpdate(data) {
       this.$emit(
         "selectionUpdate",
-        data.map((row) => row.email)
+        data.map((row) => row.name)
       );
     },
   },
