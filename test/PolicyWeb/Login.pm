49b2fe1acfcd6be330f5edae2f6c3d7b9c12a4c9
Daniel Brunkhorst <daniel.brunkhorst@dataport.de>
2024-10-18 17:11:12 +0200

    Change path to index.html

--- test/PolicyWeb/Login.pm
+++ test/PolicyWeb/Login.pm
@@ -6,29 +6,34 @@ use Test::More;
 # Argument 0 => The browser for the tests
 sub test {
     my ($browser) = @_;
-    subtest 'Load page' => sub {test_load($browser)};
-    subtest 'login elements' => sub {test_find_login($browser)};
-    subtest 'fail to login' => sub {test_fail_login($browser)};
-    subtest 'login as netspoc.test' => sub {test_login($browser)};
-    subtest 'top elements' => sub {test_find_top_buttons($browser)};
+    subtest 'Load page'             => sub { test_load($browser) };
+    subtest 'login elements'        => sub { test_find_login($browser) };
+    subtest 'fail to login'         => sub { test_fail_login($browser) };
+    subtest 'login as netspoc.test' => sub { test_login($browser) };
+    subtest 'top elements'          => sub { test_find_top_buttons($browser) };
 }
 
 # This function loads the web application
 # Argument 0 => The browser for the tests
 sub test_load {
     my ($browser) = @_;
-    $browser->get('/index.html');
+    $browser->get('/netspocvue/index.html');
     sleep 1;
-    ok($browser->get_current_url() =~ '/login', 'Login page');
+    ok( $browser->get_current_url() =~ '/login', 'Login page' );
 }
 
 # This function searches the login elements
 # Argument 0 => The browser for the tests
 sub test_find_login {
     my ($browser) = @_;
-    ok(1 == scalar @{$browser->find_elements('//input[@id="txt_login"]')}, 'Found input text login');
-    ok(1 == scalar @{$browser->find_elements('//input[@id="txt_password"]')}, 'Found input text password');
-    ok(1 == scalar @{$browser->find_elements('//button[@id="btn_login"]')}, 'Found button login');
+    ok( 1 == scalar @{ $browser->find_elements('//input[@id="txt_login"]') },
+        'Found input text login' );
+    ok(
+        1 == scalar @{ $browser->find_elements('//input[@id="txt_password"]') },
+        'Found input text password'
+    );
+    ok( 1 == scalar @{ $browser->find_elements('//button[@id="btn_login"]') },
+        'Found button login' );
 }
 
 # This function tests a login with wrong credentials
@@ -38,10 +43,9 @@ sub test_fail_login {
     $browser->find_element('//input[@id="txt_login"]')->send_keys('not-guest');
     $browser->find_element('//input[@id="txt_password"]')->send_keys('qwertz');
     $browser->find_element('//button[@id="btn_login"]')->click();
-    sleep 1;
     $browser->find_element('//button[@id="error_ok_btn"]')->click();
-    ok($browser->get_current_url() =~ '/login', 'Login as not-guest failed');
-    $browser->clear_input($browser->find_element('//input[@id="txt_login"]'));
+    ok( $browser->get_current_url() =~ '/login', 'Login as not-guest failed' );
+    $browser->clear_input( $browser->find_element('//input[@id="txt_login"]') );
 }
 
 # This function tests a login with correct credentials
@@ -51,7 +55,7 @@ sub test_login {
     $browser->find_element('//input[@id="txt_login"]')->send_keys('guest');
     $browser->find_element('//button[@id="btn_login"]')->click();
     sleep 1;
-    ok($browser->get_current_url() =~ '/services/0', 'Login as guest');
+    ok( $browser->get_current_url() =~ '/services/0', 'Login as guest' );
     $browser->select_policy('p1');
 }
 
@@ -59,12 +63,24 @@ sub test_login {
 # Argument 0 => The browser for the tests
 sub test_find_top_buttons {
     my ($browser) = @_;
-    ok(1 == scalar @{$browser->find_elements('//a[@id="tab_services"]')}, 'Found button services tab');
-    ok(1 == scalar @{$browser->find_elements('//a[@id="tab_networks"]')}, 'Found button networks tab');
-    ok(1 == scalar @{$browser->find_elements('//a[@id="tab_diff"]')}, 'Found button differences tab');
-    ok(1 == scalar @{$browser->find_elements('//input[@id="combo_policy"]')}, 'Found combo box policy');
-    ok(1 == scalar @{$browser->find_elements('//input[@id="combo_owner"]')}, 'Found combo box owner');
-    ok(1 == scalar @{$browser->find_elements('//button[@id="btn_logout"]')}, 'Found button logout');
+    ok( 1 == scalar @{ $browser->find_elements('//a[@id="tab_services"]') },
+        'Found button services tab' );
+    ok( 1 == scalar @{ $browser->find_elements('//a[@id="tab_networks"]') },
+        'Found button networks tab' );
+    ok( 1 == scalar @{ $browser->find_elements('//a[@id="tab_diff"]') },
+        'Found button differences tab' );
+    ok(
+        1 == scalar @{ $browser->find_elements('//input[@id="combo_policy"]') },
+        'Found combo box policy'
+    );
+    ok(
+        1 == scalar @{ $browser->find_elements('//input[@id="combo_owner"]') },
+        'Found combo box owner'
+    );
+    ok(
+        1 == scalar @{ $browser->find_elements('//button[@id="btn_logout"]') },
+        'Found button logout'
+    );
 }
 
 1;
